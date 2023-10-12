c8d1e7375709e3bd422f49501f37b4cddbc5d376
tyagidevansh <devanshtyagi0607@gmail.com>
2023-10-12 23:11:13 +0530

    Project 4 complete

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,57 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+//set n to 8192
+//if keyboard input is 0, set screen to white by setting all the screen registers one by one to M = 0
+//else set screen to black by setting all the screen registers to M = -1
+//do the color changing by setting ram[screen + i] = value and increment i each time
+
+
+(START)
+    @8192
+    D = A //load 8192 into D
+    @n
+    M = D //set n register to D (8192)
+
+    @i
+    M = 0 //set i to 0
+
+(LOOP)
+    @i
+    D = M //load value of i into D
+    @n
+    D = D - M //if n == i, goto end
+    @END
+    D;JEQ
+
+    @KBD
+    D = M //if keyboard is 0 ie no input, jump to white else continue
+    @WHITE
+    D;JEQ
+
+(BLACK)
+    @i
+    D = M
+    @SCREEN
+    A = A + D //set current register to start of screen + current value of i
+    M = -1 //set that register to -1
+    @INCREMENT
+    0;JMP
+
+(WHITE)
+    @i
+    D = M   
+    @SCREEN
+    A = A + D
+    M = 0  //set screen registers to 0 like black no need to jump to increment its right there
+
+(INCREMENT)
+    @i
+    M = M + 1
+    @LOOP //increment i and jump back to the loop
+    0;JMP
+
+(END)
+    @START
+    0;JMP
--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -10,3 +10,39 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+//set i and r2 to 0
+//start a for loop where at first i = 0 but at every iteration i gets incremented, when i = r1, finish the program
+//every iteration of the loop keep adding the value of r0 to r2
+
+//i = 0;
+@i  
+M = 0  //RAM[M] = 0
+//R2 = 0
+@R2
+M = 0
+
+(LOOP)
+    // if i == R1 goto END
+	@i  
+	D = M //D = RAM[M], 0 on the first iteration
+	@R1 
+	D = D - M //data at register i - data at register r1
+	@i
+	M = M + 1;
+	@END
+	D;JEQ
+
+	@R0
+	D = M
+	@R2
+	M = M + D
+
+	@LOOP
+	0; JMP
+
+(END)
+	@END
+	0;JMP
+
+	
